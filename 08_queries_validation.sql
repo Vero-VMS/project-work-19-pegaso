7013cb4cb6613a82635609def490130cd5429105
Veronica Scotti <veronica.maria.scotti@gmail.com>
2026-02-18 16:01:32 +0100

    Update and rename 05_queries_validation.sql to 08_queries_validation.sql

--- 08_queries_validation.sql
+++ 08_queries_validation.sql
@@ -1,5 +1,5 @@
 -- =========================================================
--- 05_queries_validation.sql
+-- 08_queries_validation.sql
 -- Query di validazione e verifica del modello dati (PW19)
 -- =========================================================
 -- Scopo: eseguire controlli rapidi per verificare:
